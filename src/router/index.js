6d3749f315dbf055c5090fb6422d2e524ff14af2
shimisunet <xxnanochan@gmail.com>
2018-08-02 23:10:22 +0900

    記事一覧と記事ページのroute設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Store from '@/store'
+
+import List from '@/components/list'
+import Article from '@/components/article'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: '/',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'list',
+      component: List,
+      beforeEnter: (to, from, next) => {
+        Store.dispatch('posts/fetchPosts')
+          .then(() => { next() })
+      }
+    },
+    {
+      path: '/article/:id',
+      name: 'article',
+      component: Article,
+      beforeEnter: (to, from, next) => {
+        const id = to.params['id']
+        Store.dispatch('posts/fetchPosts', id)
+          .then(() => { next() })
+      }
     }
   ]
 })
