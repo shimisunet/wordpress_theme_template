a75ddf8dbc4904804f150281339ad3e336fe3a2a
shimisunet <xxnanochan@gmail.com>
2018-08-04 22:16:14 +0900

    componentsで直接apiを叩くようにした

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
-    <h1>記事一覧</h1>
-    <div v-for="post in posts" :key="post.id">
+  <div class="post_list" v-if="posts && posts.length">
+    <div class="item" v-for="(post, index) of posts" :key="index">
       <router-link :to="`/article/${ post.id }`">{{ post.title.rendered }}</router-link>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import axios from 'axios'
 
 export default {
   name: 'list',
   data () {
     return {
+      posts: []
     }
   },
-  computed: {
-    ...mapGetters({
-      'posts': 'posts/posts'
-    })
-  },
-  methods: {
+  created () {
+    axios.get('http://shimisu.net/wp-json/wp/v2/posts/')
+      .then(response => {
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
   }
 }
 </script>
