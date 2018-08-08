70954bc79d363470ff0c3b063df4b4f994848262
shimisunet <xxnanochan@gmail.com>
2018-08-08 17:29:03 +0900

    ファイル名を変更

--- build/utils.js
+++ build/utils.js
@@ -64,7 +64,7 @@ exports.cssLoaders = function (options) {
         {
             loader: 'sass-resources-loader',
             options: {
-                resources: path.resolve(__dirname, '../src/assets/sass/_styles.scss')
+                resources: path.resolve(__dirname, '../src/assets/sass/config.scss')
             }
         }
     ),
