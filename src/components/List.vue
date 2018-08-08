5c4291dd8f1e3fae8c52dcbb8b296c6348e559e5
shimisunet <xxnanochan@gmail.com>
2018-08-09 00:41:50 +0900

    dateタグで時間を表示

--- src/components/List.vue
+++ src/components/List.vue
@@ -5,6 +5,7 @@
         <img :src="post._embedded['wp:featuredmedia'][0].source_url" alt="" class="thumbnail">
       </div>
       <router-link :to="`/article/${ post.id }`">{{ post.title.rendered }}</router-link>
+      <date>{{ post.date }}</date>
     </div>
   </div>
 </template>
