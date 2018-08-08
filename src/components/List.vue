8427ac348df4d1b229f1511fa247039e5e88b0d2
shimisunet <xxnanochan@gmail.com>
2018-08-08 15:08:52 +0900

    アイキャッチがあるときは表示させるように

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="post_list" v-if="posts && posts.length">
     <div class="item" v-for="(post, index) of posts" :key="index">
+      <div v-if="post._embedded['wp:featuredmedia']">
+        <img :src="post._embedded['wp:featuredmedia'][0].source_url" alt="" class="">
+      </div>
       <router-link :to="`/article/${ post.id }`">{{ post.title.rendered }}</router-link>
     </div>
   </div>
@@ -17,7 +20,7 @@ export default {
     }
   },
   created () {
-    axios.get('http://shimisu.net/wp-json/wp/v2/posts/')
+    axios.get('http://shimisu.net/wp-json/wp/v2/posts?_embed&per_page=100')
       .then(response => {
         this.posts = response.data
       })
